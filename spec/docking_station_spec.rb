62dca2452b7e0a36ff4308c5b37880f4a22664f2
keith(mac) <ka33145@googlemail.com>
2017-09-05 23:33:00 +0100

    Refactored docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,24 @@
 require './lib/docking_station'
 require './lib/bike'
 describe Docking_Station do
+  describe "#release_bike" do
   it "has method release_bike" do
     is_expected.to respond_to(:release_bike)
   end
+end
   it 'check if release bike is working' do
     bike = subject.release_bike
     expect(bike.working?).to eq true
   end
-  it 'has method dock' do
-    is_expected.to respond_to(:dock).with(1).argument
+  describe "#dock" do
+    it 'has method dock' do
+      is_expected.to respond_to(:dock).with(1).argument
+    end
   end
   it 'see Bike from docking station' do
-    subject.dock(Bike.new)
-    except(subject.bike).to eq (Bike.new)
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq (bike)
   end
 
 end
