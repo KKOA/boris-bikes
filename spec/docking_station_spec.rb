0fbc754f9c809f996720bd2203c8e6b2b09046a9
keith(mac) <ka33145@googlemail.com>
2017-09-05 18:37:46 +0100

    Added test to check released bike is working to docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,9 @@ describe Docking_Station do
   it "has method release_bike" do
     is_expected.to respond_to(:release_bike)
   end
+  it 'check if release bike is working' do
+    bike = subject.release_bike
+    expect(bike).to eq true
+  end
 
 end
