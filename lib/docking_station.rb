346f4be267f3fca7d8841e664d71b7c3bf50211e
keith(mac) <ka33145@googlemail.com>
2017-09-06 15:59:34 +0100

    Create private method empty_station? and full_station? and refactored docking_station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,27 @@
-class Docking_Station
-
-MAX_CAPACITY = 20
+class DockingStation
+  MAX_CAPACITY = 20
   attr_reader :bikes
   def initialize
     @bikes = []
   end
 
   def release_bike
-    raise "Docking station empty" if @bikes == []#guard condition
+    raise 'Docking station empty' if empty_station? # guard condition
     @bikes.pop
   end
 
   def dock(bike)
-    raise "Docking station is full" if @bikes.count >=  MAX_CAPACITY #guard condition
+    raise 'Docking station is full' if full_station? # guard condition
     @bikes << bike
+  end
+
+  private
+
+  def empty_station?
+    @bikes.empty?
+  end
+
+  def full_station?
+    @bikes.count >= MAX_CAPACITY
   end
 end
