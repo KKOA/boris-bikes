22ecb4e3746e49d921a7f0f76e0539e332b31207
keith(mac) <ka33145@googlemail.com>
2017-09-06 13:08:29 +0100

    Added guard condition to dock and refactored error messages in docking_station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,14 +2,12 @@ class Docking_Station
 
   attr_reader :bike
   def release_bike
-    raise "message" unless @bike
+    raise "Docking station empty" unless @bike #guard condition
     @bike
   end
 
   def dock(bike)
+    raise "Docking staion is full" if @bike #guard condition
     @bike = bike
   end
-
-
-
 end
